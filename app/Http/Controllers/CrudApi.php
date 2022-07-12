fe54e94eec454f42b0eb2899486ccf47ec6764e5
olashina201 <olashina201@gmail.com>
2022-07-12 16:51:25 +0100

    post request

--- app/Http/Controllers/CrudApi.php
+++ app/Http/Controllers/CrudApi.php
@@ -8,11 +8,26 @@ use App\Models\PhoneBook;
 class CrudApi extends Controller
 {
     //
-    function list() {
+    function list()
+    {
         return PhoneBook::all();
     }
 
-    function getSinglePhoneBook($id) {
+    function getSinglePhoneBook($id)
+    {
         return PhoneBook::find($id);
+    }
+
+    function addPhoneBook(Request $req)
+    {
+        $phoneBook = new PhoneBook;
+        $phoneBook->name = $req->name;
+        $phoneBook->age = $req->age;
+        $phoneBook->contact = $req->contact;
+        $result = $phoneBook->save();
+        if ($result) {
+            return ["Result" => $result];
+        }
+        return ["Result" => "and error occured"];
     }
 }
