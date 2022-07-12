b5a53db831d1723930977f300284b7e818ab43fb
olashina201 <olashina201@gmail.com>
2022-07-12 16:24:11 +0100

    migrate database

--- app/Http/Controllers/CrudApi.php
+++ app/Http/Controllers/CrudApi.php
@@ -3,8 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\PhoneBook;
 
 class CrudApi extends Controller
 {
     //
+    function list() {
+        return PhoneBook::all();
+    }
 }
