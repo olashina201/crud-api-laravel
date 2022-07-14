0b187efb37a0db1f646ce2ddb7f19eb04fa890b7
olashina201 <olashina201@gmail.com>
2022-07-14 22:22:17 +0100

    migration

--- app/Models/PhoneBook.php
+++ app/Models/PhoneBook.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class PhoneBook extends Model
 {
     use HasFactory;
+    
+    protected $guarded = [];
 }
---
+++ database/migrations/2022_07_14_211424_phone_book.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('phone_books', function (Blueprint $table) {
+            $table->id();
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->int('contact');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
